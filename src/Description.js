7b3e297504d8f1ea3f8e8f9084378f323ee925a1
tze-tan <59767908+tze-tan@users.noreply.github.com>
2024-01-26 16:59:13 +0100

    updating structure and styles of description

--- src/Description.js
+++ src/Description.js
@@ -8,33 +8,34 @@ export default function Description() {
         <h1 id="city" className="city">
           Munich
         </h1>
-        <div className="temperature-container">
-          <div id="temp-image" className="temp-image">
+        <div className="row">
+          <p>Tuesday, 23:33</p>
+        </div>
+        <div className="row">
+          <div className="col-6">
             <img
               src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
               className="temp-image"
-              alt-text=""
+              alt-text="temperature"
             />
+
+            <span className="current-temperature" id="current-temperature">
+              24
+            </span>
+            <span className="temperature-celcius">°C</span>
           </div>
-          <div className="current-temperature" id="current-temperature">
-            24
+
+          <div className="col-6">
+            <ul>
+              <li>Description: {""} Overcast clouds</li>
+              <li>
+                Humidity: {""}
+                15%
+              </li>
+              <li>Windspeed: 65km/h</li>
+            </ul>
           </div>
-          <div className="temperature-celcius">°C</div>
         </div>
-      </div>
-      <div>
-        <p>
-          <span id="dayintheweek">Tuesday</span>, <span id="time">23:33</span>,
-          <span id="description"> overcast clouds</span> <br />
-          Humidity: {""}
-          <span id="humidity" className="humidity">
-            15%
-          </span>{" "}
-          Windspeed:{" "}
-          <span id="windspeed" className="windspeed">
-            65km/h
-          </span>
-        </p>
       </div>
     </div>
   );
